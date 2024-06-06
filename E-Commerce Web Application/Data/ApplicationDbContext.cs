4d5b580a5d2c7c3c76352d12ee0024e051e16f01
VuKhoa23 <vuanhkhoa007@gmail.com>
2024-06-06 10:58:26 +0700

    Update main branch

---
+++ E-Commerce Web Application/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce_Web_Application.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
