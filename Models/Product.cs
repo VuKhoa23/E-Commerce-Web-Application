d19a6edceebc9cc0900d25620fd5af89fc9aae91
VuKhoa23 <vuanhkhoa007@gmail.com>
2024-06-10 10:51:16 +0700

    Create product model

---
+++ Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        [Display(Name = "List Price")]
+        [Range(1, 1000)]
+        public double ListPrice { get; set; }
+    }
+}
